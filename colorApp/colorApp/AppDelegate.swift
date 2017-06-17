01ec8e6c4f5bba97a7bbcde238091bdcd4564288
Ibrahimhass <mdibrahimhassan@gmail.com>
2017-06-18 05:00:42 +0530

    Seaching, Swapping and Sorting Basics

---
+++ colorApp/colorApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  colorApp
+//
+//  Created by Md Ibrahim Hassan on 13/06/17.
+//  Copyright © 2017 Md Ibrahim Hassan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
